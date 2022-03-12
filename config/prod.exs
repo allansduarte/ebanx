166af1bc72c62c87cd0102f3cde926ef3196e638
Allan <allan.sduarte@gmail.com>
2022-03-12 13:41:24 -0300

    add ssl

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :ebanx_web, EbanxWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "https://whispering-oasis-99694.herokuapp.com/", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # ## SSL Support
 #
