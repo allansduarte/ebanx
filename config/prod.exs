c521422b2ccb8d0d89dc3bdcc5c88d7622f01347
Allan <allan.sduarte@gmail.com>
2022-03-12 14:28:35 -0300

    change prod port

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :ebanx_web, EbanxWeb.Endpoint,
-  url: [host: "https://whispering-oasis-99694.herokuapp.com/", port: 443],
+  url: [host: "https://whispering-oasis-99694.herokuapp.com/", port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
